3ee318e7c3459f4fca635b95f7a4a844902aade1
pinky-pig <2545589993@qq.com>
2023-03-27 14:41:44 +0800

    chore: 删除 component folder

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,38 +1,8 @@
 <script setup lang="ts">
-import { defineComponent, h, markRaw } from 'vue'
-// import V3bento from '../../packages/v3-bento/src'
-import V3bento from '../../packages/v3-bento/dist/v3-bento.es.js'
+import V3bento from '../../packages/v3-bento/src'
+import { cfg } from './bento-components'
+// import V3bento from '../../packages/v3-bento/dist/v3-bento.es.js'
 
-const cfg = [
-  { id: '1', x: 0, y: 0, width: 1, height: 1, component: markRaw(createBentoCellComponent('Bento1', '1')) },
-  { id: '2', x: 1, y: 0, width: 1, height: 1, component: markRaw(createBentoCellComponent('Bento2', '2')) },
-  { id: '3', x: 2, y: 0, width: 2, height: 2, component: markRaw(createBentoCellComponent('Bento3', '3')) },
-  { id: '4', x: 3, y: 0, width: 2, height: 2, component: markRaw(createBentoCellComponent('Bento4', '4')) },
-  { id: '5', x: 2, y: 1, width: 2, height: 1, component: markRaw(createBentoCellComponent('Bento5', '5')) },
-  { id: '6', x: 2, y: 1, width: 2, height: 1, component: markRaw(createBentoCellComponent('Bento6', '6')) },
-]
-
-// 创建组件
-const colors = 'cae7b9-f3de8a-eb9486-7e7f9a-97a7b3-4059ad'.split('-').map(a => `#${a}`)
-function createBentoCellComponent(name: string, content: string) {
-  return defineComponent({
-    name,
-    setup() {
-      return () => h(
-        'div',
-        {
-          style: `
-            background: ${colors[Number(content) - 1]};
-            display: grid;
-            place-items: center;
-            font-size: 48px;
-            user-select: none;
-          `,
-        },
-        content)
-    },
-  })
-}
 const print = (val: string, e: any) => {
   // eslint-disable-next-line no-console
   console.log(val, e)
