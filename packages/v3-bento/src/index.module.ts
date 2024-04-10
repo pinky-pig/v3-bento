6a0e847cf24b64bfbf51497363e55a301f18a352
pinky-pig <2545589993@qq.com>
2024-04-10 11:56:06 +0800

    fix: 默认排序顺序安装实际布局顺序

--- packages/v3-bento/src/index.module.ts
+++ packages/v3-bento/src/index.module.ts
@@ -188,6 +188,13 @@ export function initGridContainer(
     }
     mouseFrom.x = 0
     mouseFrom.y = 0
+    // 拖拽完成后，数组按照布局顺序排列
+    bentoCells.value = bentoCells.value.sort((a, b) => {
+      if (a.y !== b.y)
+        return a.y - b.y // 按照 y 值升序排序
+      else
+        return a.x - b.x // 在同一行内，按照 x 值升序排序
+    })
     isDragging.value = false
   }
 
@@ -212,30 +219,24 @@ export function isNeedDefaultLayout(bentoCells: Ref<BentoCellsType[]>, propsOpti
     sortDefault(bentoCells, propsOption.maximumCells)
   }
 }
+// 默认布局
 function sortDefault(bentoCells: Ref<BentoCellsType[]>, maximumCells: number) {
   const totalHeight = bentoCells.value.reduce((sum, item) => sum + item.y + item.height, 0)
   const chessboard: number[][] = new Array(totalHeight).fill(0).map(() => new Array(maximumCells).fill(0))
 
-  const cell11: BentoCellsType[] = []
-  const cell12: BentoCellsType[] = []
-  const cell21: BentoCellsType[] = []
-  const cell22: BentoCellsType[] = []
-  bentoCells.value.forEach((item, index) => {
-    if (item.width === 1 && item.height === 1)
-      cell11.push(item)
-    else if (item.width === 1 && item.height === 2)
-      cell12.push(item)
-    else if (item.width === 2 && item.height === 1)
-      cell21.push(item)
-    else if (item.width === 2 && item.height === 2)
-      cell22.push(item)
+  /**
+   * 1. 先按顺序排列
+   * 2. 再一个一个排序
+   */
+
+  bentoCells.value = bentoCells.value.sort((a, b) => {
+    if (a.y !== b.y)
+      return a.y - b.y // 按照 y 值升序排序
+    else
+      return a.x - b.x // 在同一行内，按照 x 值升序排序
   })
 
-  // 1. 先把 2*2 的放进去
-  // 2. 再把 2*1 的放进去
-  // 3. 再把 1*2 的放进去
-  // 4. 最后把 1*1 的放进去
-  for (let index = 0; index < cell22.length; index++) {
+  for (let index = 0; index < bentoCells.value.length; index++) {
     let foundCell = false // 因为 break 只能跳出单个循环，所以用这个变量来跳出双层循环
 
     for (let row = 0; row < chessboard.length; row++) {
@@ -243,83 +244,53 @@ function sortDefault(bentoCells: Ref<BentoCellsType[]>, maximumCells: number) {
         break
 
       for (let col = 0; col < chessboard[row].length; col++) {
-        if (
-          chessboard[row][col] === 0
-          && chessboard[row][col + 1] === 0
-          && chessboard[row + 1][col] === 0
-          && chessboard[row + 1][col + 1] === 0
-        ) {
-        // 说明是空的
-          cell22[index].x = col
-          cell22[index].y = row
-          chessboard[row][col] = 1
-          chessboard[row][col + 1] = 1
-          chessboard[row + 1][col] = 1
-          chessboard[row + 1][col + 1] = 1
-          foundCell = true
-          break
+        // 2x2
+        if (bentoCells.value[index].width === 2 && bentoCells.value[index].height === 2) {
+          if (chessboard[row][col] === 0
+            && chessboard[row][col + 1] === 0
+            && chessboard[row + 1][col] === 0
+            && chessboard[row + 1][col + 1] === 0) {
+            bentoCells.value[index].x = col
+            bentoCells.value[index].y = row
+            chessboard[row][col] = 1
+            chessboard[row][col + 1] = 1
+            chessboard[row + 1][col] = 1
+            chessboard[row + 1][col + 1] = 1
+            foundCell = true
+            break
+          }
         }
-      }
-    }
-  }
-  for (let index = 0; index < cell21.length; index++) {
-    let foundCell = false // 因为 break 只能跳出单个循环，所以用这个变量来跳出双层循环
-
-    for (let row = 0; row < chessboard.length; row++) {
-      if (foundCell)
-        break
-
-      for (let col = 0; col < chessboard[row].length; col++) {
-        if (chessboard[row][col] === 0
-          && chessboard[row][col + 1] === 0
-        ) {
-        // 说明是空的
-          cell21[index].x = col
-          cell21[index].y = row
-          chessboard[row][col] = 1
-          chessboard[row][col + 1] = 1
-          foundCell = true
-          break
+        // 2x1
+        if (bentoCells.value[index].width === 2 && bentoCells.value[index].height === 1) {
+          if (chessboard[row][col] === 0 && chessboard[row][col + 1] === 0) {
+            bentoCells.value[index].x = col
+            bentoCells.value[index].y = row
+            chessboard[row][col] = 1
+            chessboard[row][col + 1] = 1
+            foundCell = true
+            break
+          }
         }
-      }
-    }
-  }
-  for (let index = 0; index < cell12.length; index++) {
-    let foundCell = false // 因为 break 只能跳出单个循环，所以用这个变量来跳出双层循环
-
-    for (let row = 0; row < chessboard.length; row++) {
-      if (foundCell)
-        break
-
-      for (let col = 0; col < chessboard[row].length; col++) {
-        if (
-          chessboard[row][col] === 0
-          && chessboard[row + 1][col] === 0
-        ) {
-          cell12[index].x = col
-          cell12[index].y = row
-          chessboard[row][col] = 1
-          chessboard[row + 1][col] = 1
-          foundCell = true
-          break
+        // 1x2
+        if (bentoCells.value[index].width === 1 && bentoCells.value[index].height === 2) {
+          if (chessboard[row][col] === 0 && chessboard[row + 1][col] === 0) {
+            bentoCells.value[index].x = col
+            bentoCells.value[index].y = row
+            chessboard[row][col] = 1
+            chessboard[row + 1][col] = 1
+            foundCell = true
+            break
+          }
         }
-      }
-    }
-  }
-  for (let index = 0; index < cell11.length; index++) {
-    let foundCell = false // 因为 break 只能跳出单个循环，所以用这个变量来跳出双层循环
-
-    for (let row = 0; row < chessboard.length; row++) {
-      if (foundCell)
-        break
-
-      for (let col = 0; col < chessboard[row].length; col++) {
-        if (chessboard[row][col] === 0) {
-          cell11[index].x = col
-          cell11[index].y = row
-          chessboard[row][col] = 1
-          foundCell = true
-          break
+        // 1x1
+        if (bentoCells.value[index].width === 1 && bentoCells.value[index].height === 1) {
+          if (chessboard[row][col] === 0) {
+            bentoCells.value[index].x = col
+            bentoCells.value[index].y = row
+            chessboard[row][col] = 1
+            foundCell = true
+            break
+          }
         }
       }
     }
